db0e5f7082e2246e32e33ad595e091da584e9c18
Mateo M <matimm@gmail.com>
2017-04-01 18:15:41 +0200

    działająca wersja. przesyła wiadomości tekstowe

---
+++ ejbModule/pakiet/BeanBolid.java
@@ -0,0 +1,83 @@
+package pakiet;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+/**
+ * Message-Driven Bean implementation class for: BeenBolid
+ */
+/*@MessageDriven(
+		activationConfig = { @ActivationConfigProperty(
+				propertyName = "destination", propertyValue = "jms.topic.sriTopic"), @ActivationConfigProperty(
+				propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+		}, 
+		mappedName = "jms.topic.sriTopic")*/
+@Startup//M
+@Singleton//M
+public class BeanBolid implements MessageListener {
+	
+	@Resource(mappedName = "java:/ConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+	@Resource(lookup =  "java:/jms.topic.sriTopic")
+    private Topic topic;
+    
+    //@Resource
+    //private MessageDrivenContext mdc;
+    
+    public BeanBolid() {
+        // TODO Auto-generated constructor stub
+    }
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)//M
+	@Schedule(minute="*/1",hour="*")//M minute="*/10"
+	@PostConstruct//M
+    public void sendMessage(){
+    	 Connection connection=null;
+		try {
+			connection = connectionFactory.createConnection();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+	         MessageProducer messageProducer = session.createProducer(topic);
+	         TextMessage message=session.createTextMessage();
+	         message.setText("WIADOMOŒÆ!");
+	         messageProducer.send(message);
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			if(connection!=null)
+				try {
+					connection.close();
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+    }
+	
+	/**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
